ab08d3151ade932f3f0ee5c69b5d12b7e499de9f
Gustavo Ortiz <gustavoortiz@Gustavos-Air.attlocal.net>
2020-04-06 20:10:10 -0400

    java challenge?

---
+++ script.js
@@ -0,0 +1,62 @@
+var Person = function (name, job, age) {
+    this.name = name;
+    this.job = job;
+    this.age = age;
+    this.profession = function(){
+    console.log(this.name + ' is a ' + this.job);
+    }
+    this.exercise = function() {
+    console.log(this.name + ' running is fun! - said no one ever');
+    }
+    this.completeTask = function() {
+    console.log(this.name + ' is busy ');
+    }
+    this.acceptTask = function() {
+    console.log(this.name + ' is not busy');
+    }   
+}
+
+var Programmer = function(language, busy){
+    Person.call(name, job, age);
+    this.language = 'javascript';
+    this.busy = true;
+    this.completeTask = function() {
+        console.log(this.name + ' is busy.');
+    }
+    this.acceptTask = function () {
+        console.log(this.name + ' is not busy.');
+    }
+    this.acceptTask = function(){
+    }
+};
+
+Programmer.prototype = Object.create(Person.prototype);
+Programmer.prototype.consotructor = Programmer;
+
+var john = new Person('John', ' is a front-end developer.', 26);
+var mary = new Person('Mary', ' is a back-end developer.', 21);
+
+john.exercise();
+mary.exercise();
+john.profession();
+mary.profession();
+john.completeTask();
+
+
+
+
+
+//HARD CHALLENGE
+function clientInfo() {
+    var pii = {
+        name: 'Jose Claudio Padín',
+        ssn: '744-58-6924'
+    }
+    function displayName() {
+        console.log(pii.name);
+    }
+    return displayName;
+}
+
+var clientData = clientInfo();
+clientData();
